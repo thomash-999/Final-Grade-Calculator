5068c171e08b69012871a72149294d5321c2d124
thomash-999 <thomashung944@gmail.com>
2026-01-05 14:55:39 -0600

    Added menu options

---
+++ dropped_test.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+#include <string> 
+#include "courses.h"
+
+using namespace std;
+
+int dropped_test() {
+    
+}
+
--- grade_calc_menu.cpp
+++ grade_calc_menu.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main() {
     int user_selection = 0;
-    cout << "1. Calculate required grade needed on final for multiple courses " << endl;
-    cout << "2. " << endl;
+    cout << "1. Calculate the grade needed on the final for multiple courses" << endl;
+    cout << "2. Calculate the grade needed on the final with a test dropped" << endl;
     cout << "3. " << endl;
     cin >> user_selection;
 
